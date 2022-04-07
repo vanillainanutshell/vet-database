907d73a371036d6645c5622bd8c5273167eaf87d
Mauro Vasconcelos <88060989+vanillainanutshell@users.noreply.github.com>
2022-04-07 18:47:30 -0300

    Add new tables

--- data.sql
+++ data.sql
@@ -1,13 +1,33 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals VALUES(1, 'Agumon', '3-FEB-2020', 0, true, 10.23);
-INSERT INTO animals VALUES(2, 'Gabumon', '15-NOV-2018', 2, true, 8);
-INSERT INTO animals VALUES(3, 'Pikachu', '7-JAN-2021', 1, false, 15.04);
-INSERT INTO animals VALUES(4, 'Devimon', '12-MAY-2017', 5, true, 11);
-INSERT INTO animals VALUES(5, 'Charmander', '8-FEB-2020', 0, false, -11.0);
-INSERT INTO animals VALUES(6, 'Plantmon', '15-NOV-2021', 2, true, -5.7);
-INSERT INTO animals VALUES(7, 'Squirtle', '2-APR-1993', 3, false, -12.13);
-INSERT INTO animals VALUES(8, 'Angemon', '12-JUN-2005', 1, true, -45.0);
-INSERT INTO animals VALUES(9, 'Boarmon', '7-JUN-2005', 7, true, 20.4);
-INSERT INTO animals VALUES(10, 'Blossom', '13-OCT-1998', 3, true, 17);
-INSERT INTO animals VALUES(11, 'Blossom', '14-MAY-2022', 4, true, 22); 
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1, 'Agumon', '3-FEB-2020', 0, true, 10.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(2, 'Gabumon', '15-NOV-2018', 2, true, 8);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(3, 'Pikachu', '7-JAN-2021', 1, false, 15.04);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(4, 'Devimon', '12-MAY-2017', 5, true, 11);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(5, 'Charmander', '8-FEB-2020', 0, false, -11.0);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(6, 'Plantmon', '15-NOV-2021', 2, true, -5.7);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(7, 'Squirtle', '2-APR-1993', 3, false, -12.13);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(8, 'Angemon', '12-JUN-2005', 1, true, -45.0);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(9, 'Boarmon', '7-JUN-2005', 7, true, 20.4);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(10, 'Blossom', '13-OCT-1998', 3, true, 17);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(11, 'Blossom', '14-MAY-2022', 4, true, 22); 
+
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
+
+UPDATE animals SET species_id = CASE WHEN name LIKE '%mon' THEN  2 ELSE 1 END;
+
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN ('Pikachu', 'Gabumon');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+
+UPDATE owners SET full_name = 'Melody Pond' WHERE id = 4;
--- schema.sql
+++ schema.sql
@@ -8,4 +8,23 @@ CREATE TABLE animals (
     neutered BOOLEAN,
     weight_kg DECIMAL
 );
-ALTER TABLE animals ADD species VARCHAR(40);
+ALTER TABLE animals ADD species VARCHAR(50);
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(50) NOT NULL,
+    age INT NOT NULL
+);
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE animals DROP species;
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD owner_id INT;
+
+ALTER TABLE animals ADD CONSTRAINT fk_owners
+FOREIGN KEY(owner_id) REFERENCES owners(id);
+ALTER TABLE animals ADD CONSTRAINT fk_species
+FOREIGN KEY(species_id) REFERENCES species(id);
